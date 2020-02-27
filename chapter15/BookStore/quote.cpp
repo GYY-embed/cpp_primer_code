407cef3f1e08cdae9010ec61e273787685b9ae6d
GYY <gyyjx123024@gmail.com>
2020-02-27 11:21:21 +0800

    ch15

---
+++ chapter15/BookStore/quote.cpp
@@ -0,0 +1,26 @@
+#include "quote.h"
+
+#include <algorithm>
+using std::min;
+
+#include <cstddef>
+using std::size_t;
+
+#include <iostream>
+using std::ostream; using std::endl; 
+using std::cout;
+
+double Bulk_quote::net_price(std::size_t cnt) const
+{
+    if(cnt >= quantity)
+        return cnt * (1 - discount) * price;
+    else
+        return cnt * price;
+}
+
+double print_total(std::ostream &os, const Quote &item, size_t n)
+{
+    double ret = item.net_price(n);
+    os << "ISBN: " << item.isbn() << "# sold: " << n << "total due: " << ret << endl;
+    return ret;
+}
--- chapter15/BookStore/quote.h
+++ chapter15/BookStore/quote.h
@@ -15,6 +15,10 @@ public:
     std::string isbn() const { return bookNo; }
     virtual double net_price(std::size_t n) const
         { return n*price; }
+    virtual void debug()
+    {
+        std::cout << "bookNo = " << bookNo << " price = " << price << std::endl;
+    }
     virtual ~Quote() = default;
 private:
     std::string bookNo;
@@ -22,15 +26,36 @@ protected:
     double price = 0.0;
 };
 
-class Bulk_quote : public Quote 
+
+class Disc_quote : public Quote
+{
+public:
+    Disc_quote() = default;
+    Disc_quote(const std::string &book, double price, std::size_t qty, double disc):
+        Quote(book, price), quantity(qty), discount(disc) { }
+    double net_price(std::size_t) const = 0;
+    std::pair<size_t, double> discount_policy() const
+        { return std::make_pair(quantity, discount); }
+protected:
+    std::size_t quantity = 0;
+    double discount = 0.0;
+};
+
+class Bulk_quote : public Disc_quote 
 {
 public:
     Bulk_quote() = default;
-    Bulk_quote(const std::string &, double, std::size_t, double);
-    double net_price(std::size_t) const override;
-private:
-    std::size_t min_qty = 0;//适用折扣政策的最低购买量
-    double discount = 0.0;//折扣额，小数
+    Bulk_quote(const std::string &book, double p, std::size_t qty, double disc):
+        Disc_quote(book, p, qty,disc) { }
+    double net_price(std::size_t) const ;
+/*     virtual void debug()
+    {
+        Quote::debug();
+        std::cout << "min_qty = " << quantity << "discount = " << discount << std::endl;
+    } */
+
 };
 
+
+double print_total(std::ostream &os, const Quote &item, size_t n);
 #endif // !QUOTE_H
